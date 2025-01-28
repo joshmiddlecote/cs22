1da2bb99583a5d07aadb1122b23814be28c58b06
joshmiddlecote <joshuam@betsoftware.com>
2025-01-28 12:21:50 +0000

    handle missing data in movies migrations

--- migrations/movies/movies.py
+++ migrations/movies/movies.py
@@ -23,6 +23,17 @@ def create_movies_table(cursor, conn):
     """)
     conn.commit()
 
+def handle_missing_data(row3):
+    _, budget, revenue, language, overview, runtime, tagline = row3
+    if budget == "":
+        budget = 0
+    if revenue == "":
+        revenue = 0
+    if runtime == "":
+        runtime =  0
+
+    return (budget, revenue, language, overview, runtime, tagline)
+
 def insert_movie_data(cursor, conn):
     with open('data/ml-latest-small/movies.csv', newline='', encoding='utf-8') as csvfile, open('data/ml-latest-small/movie_average_ratings.csv', newline='', encoding='utf-8') as ratings_csvfile, open('data/ml-latest-small/movie_extra_details.csv', newline='', encoding='utf-8') as extra_details_csvfile:
         movie_csvreader = csv.reader(csvfile)
@@ -35,7 +46,7 @@ def insert_movie_data(cursor, conn):
         for row1, row2, row3 in zip(movie_csvreader, ratings_csvreader, extra_details_csvreader):
             movie_id, title, genres = row1
             _, average_rating, num_ratings = row2
-            _, budget, revenue, language, overview, runtime, tagline = row3
+            (budget, revenue, language, overview, runtime, tagline) = handle_missing_data(row3)
             year = re.search(r'\((\d{4})\)', title).group(1)
             title = re.sub(r'\s*\(\d{4}\)', '', title)
 
