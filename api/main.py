1dbc456a94ce5761e33cf77f91f36284ef89b4f6
joshmiddlecote <joshuam@betsoftware.com>
2025-01-29 23:38:42 +0000

    add nicer html and paging for movies

--- api/main.py
+++ api/main.py
@@ -1,16 +1,25 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Query, Request
 from fastapi.templating import Jinja2Templates
-import queries
+from fastapi.responses import RedirectResponse
+import movies.queries as movie_queries
 
 app = FastAPI()
 templates = Jinja2Templates(directory="../templates")
 
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/movies")
+
 @app.get("/movies")
-def read_root(request: Request):
-    movies = queries.get_all_movies()
-    return templates.TemplateResponse("movies.html", {"request": request, "movies": movies})
+def read_root(request: Request, page: int = Query(1, ge=1), size: int = Query(10, le=100)):
+    start_index = (page - 1) * size
+    (movies, total_pages) = movie_queries.get_all_movies(size, start_index)
+    return templates.TemplateResponse("index.html", 
+        {"request": request, "movies": movies, "page": page, 
+         "size": size, "total_pages": total_pages})
 
 @app.get("/movies/{movie_id}")
 def read_movie(request: Request, movie_id: int):
-    movie = queries.get_movie_by_movie_id(movie_id)
-    return templates.TemplateResponse("movies.html", {"request": request, "movies": movie})
+    movie = movie_queries.get_movie_by_movie_id(movie_id)
+    return templates.TemplateResponse("movie.html", {"request": request, "movie": movie})
+
--- api/movies/queries.py
+++ api/movies/queries.py
@@ -17,15 +17,22 @@ def get_db():
     finally:
         conn.close()
 
-def get_all_movies():
+def get_all_movies(limit=10, offset=0):
     with get_db() as conn:
         with conn.cursor() as cursor:
             cursor.execute("""
-                SELECT * FROM movies;
-            """)
+                SELECT * FROM movies LIMIT %s OFFSET %s;
+            """, (limit, offset))
 
             movies = cursor.fetchall()
-            return [{'id': movie[0], 'title': movie[1], 'runtime': movie[3], 'overview': movie[11]} for movie in movies]
+
+            cursor.execute("SELECT COUNT(*) FROM movies;")
+            total_movies = cursor.fetchone()[0]
+            
+            total_pages = (total_movies + limit - 1) // limit if limit > 0 else 0
+            movies =  [{'id': movie[0], 'title': movie[1], 'runtime': movie[3], 'overview': movie[11]} for movie in movies]
+
+            return (movies, total_pages)
         
 def get_movie_by_movie_id(id):
     with get_db() as conn:
@@ -35,4 +42,9 @@ def get_movie_by_movie_id(id):
             """, (str(id)))
 
             movie = cursor.fetchone()
-            return [{'id': movie[0], 'title': movie[1], 'runtime': movie[3], 'overview': movie[11]}]
+            return {'id': movie[0], 'title': movie[1], 'year_released': movie[2], 
+                    'runtime': movie[3], 'director': movie[4], 'language_id': movie[5], 
+                    'average_rating': movie[6], 'num_ratings': movie[7],
+                    'budget': movie[9], 'revenue': movie[10],
+                    'overview': movie[11], 'tagline': movie[12]}
+        
