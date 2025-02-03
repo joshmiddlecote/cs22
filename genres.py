df10c108b9ef6d4a8447cd703d3e03ac2f3ece22
Lucia Kim <kimseungyounglucia@gmail.com>
2025-02-03 13:28:23 +0000

    Generate user rating data by genre and for all movies

--- genres.py
+++ genres.py
@@ -3,13 +3,11 @@ import csv
 import os 
 import re
 from dotenv import load_dotenv
-from collections import defaultdict
-import statistics
 
 
 def drop_tables(cursor, conn):
     cursor.execute("""
-        DROP TABLE IF EXISTS genres_link, movies, genres CASCADE;
+        DROP TABLE IF EXISTS genre_links, movies, genres CASCADE;
     """)
     conn.commit()
 
@@ -17,7 +15,7 @@ def drop_tables(cursor, conn):
 def create_genres_table(cursor, conn):
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS genres(
-            id SERIAL PRIMARY KEY,
+            id INT PRIMARY KEY,
             genre_name VARCHAR(255) NOT NULL, 
             avg_rating FLOAT DEFAULT 0, 
             variance FLOAT DEFAULT 0, 
@@ -27,9 +25,9 @@ def create_genres_table(cursor, conn):
     conn.commit()
 
 
-def create_genres_link_table(cursor, conn):
+def create_genre_links_table(cursor, conn):
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS genres_link(
+        CREATE TABLE IF NOT EXISTS genre_links(
             movie_id INT REFERENCES movies(id),
             genre_id INT REFERENCES genres(id),
             PRIMARY KEY (movie_id, genre_id)
@@ -49,20 +47,21 @@ def create_movies_table(cursor, conn):
     conn.commit()
 
 
-def insert_unique_genres(cursor, conn):
-    unique_genres = set()
-    with open('data/ml-latest-small/movies.csv', 'r') as file:
+def insert_genres(cursor, conn):
+    with open('data/ml-latest-small/genres.csv', 'r') as file:
         reader = csv.DictReader(file)
         
         for row in reader:
-            genres = row['genres'].split('|')  # splits genres by '|'
-            unique_genres.update(genres)      # add genres to set, ignoring duplicates
+            id = row['id']
+            genre_name = row['genre_name']
+            avg_rating = row['avg_rating']
+            variance = row['variance']
+            total_ratings = row['total_ratings']
 
-    for genre in unique_genres:
-        cursor.execute("""
-            INSERT INTO genres (genre_name)
-            VALUES (%s);
-        """, (genre,))  # ON CONFLICT to skip duplicates
+            cursor.execute("""
+                INSERT INTO genres (id, genre_name, avg_rating, variance, total_ratings)
+                VALUES (%s, %s, %s, %s, %s);
+            """, (id, genre_name, avg_rating, variance, total_ratings)) 
 
     conn.commit()
 
@@ -84,67 +83,18 @@ def insert_movies(cursor, conn):
 
 
 def insert_genre_links(cursor, conn):
-    with open('data/ml-latest-small/movies.csv', 'r') as file:
+    with open('data/ml-latest-small/genre_links.csv', 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            movie_id = int(row['movieId'])
-            genres = row['genres'].split('|')
+            movie_id = int(row['movie_id'])
+            genre_id = int(row['genre_id'])
 
             cursor.execute("""
-                SELECT id FROM movies WHERE id = %s;
-            """, (movie_id,))
-            movie_id_primary = cursor.fetchone()  # Fetch one result
-
-            for genre in genres:
-
-              cursor.execute("""
-                  SELECT id FROM genres WHERE genre_name = %s;
-                """, (genre,))
-              genre_id_primary = cursor.fetchone()
-
-              cursor.execute("""
-                  INSERT INTO genres_link(movie_id, genre_id)
-                  VALUES (%s, %s)
-              """, (movie_id_primary, genre_id_primary))
+                INSERT INTO genre_links(movie_id, genre_id)
+                VALUES (%s, %s)
+            """, (movie_id, genre_id))
 
         conn.commit()
-
-
-def get_genre_ratings_data(cursor, conn):
-    ratings_by_genre = defaultdict(list)
-
-    with open('data/ml-latest-small/ratings.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            movie_id = int(row['movieId'])
-            rating = float(row['rating'])
-
-            cursor.execute("""
-                SELECT genre_id FROM genres_link
-                WHERE movie_id = %s;
-            """, (movie_id,))
-            genre_ids = cursor.fetchall() # list of all genres the movie belongs to 
-            genre_ids = [id[0] for id in genre_ids]
-
-            for genre_id in genre_ids:
-                ratings_by_genre[int(genre_id)].append(rating)
-    # print('RATINGS BY GENRE: ', ratings_by_genre)
-    insert_genre_ratings_data(cursor, conn, ratings_by_genre)
-
-
-def insert_genre_ratings_data(cursor, conn, ratings_by_genre):
-    for genre_id, ratings_list in ratings_by_genre.items():
-        num_ratings = len(ratings_list)
-        avg_rating = sum(ratings_list) / num_ratings 
-        variance = statistics.variance(ratings_list)
-
-        cursor.execute("""
-            UPDATE genres
-            SET avg_rating = %s, variance = %s, total_ratings = %s
-            WHERE id = %s;
-        """, (avg_rating, variance, num_ratings, genre_id))
-    print('Finished inserting genre ratings data.')
-    conn.commit()
 
 
 def main():
@@ -168,23 +118,21 @@ def main():
         cursor = conn.cursor()
 
         # delete any existing tables
-        # drop_tables(cursor, conn)
+        drop_tables(cursor, conn)
 
         # create tables
         create_movies_table(cursor, conn)
         create_genres_table(cursor, conn)
-        create_genres_link_table(cursor, conn)
+        create_genre_links_table(cursor, conn)
         print('Finished creating all tables')
 
         # insert data
-        # insert_unique_genres(cursor, conn)
-        # print('Finished inserting genres id + name data.')
-        # insert_movies(cursor, conn)
-        # print('Finished inserting movies data.')
-        # insert_genre_links(cursor, conn)
-        # print('Finished inserting genre_links data.')
-
-        get_genre_ratings_data(cursor, conn)
+        insert_movies(cursor, conn)
+        print('Finished inserting movies data.')
+        insert_genres(cursor, conn)
+        print('Finished inserting genres data')
+        insert_genre_links(cursor, conn)
+        print('Finished inserting genre_links data.')
 
     except Exception as e:
         print(f"Error occurred: {e}")
