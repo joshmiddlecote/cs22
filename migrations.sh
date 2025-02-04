7552ffa9769bff2e98a4e70968199be73f84da2d
joshmiddlecote <joshuam@betsoftware.com>
2025-02-04 14:14:49 +0000

    add all migrations to migrations.sh

--- migrations.sh
+++ migrations.sh
@@ -1,11 +1,13 @@
-docker-compose up -d
-
-s
-
 echo "Migrations running..."
 
 python3 migrations/languages/languages.py
 python3 migrations/movies/movies.py
+python3 migrations/genres/genres.py
+python3 migrations/awards/load_award_awardid.py
+python3 migrations/awards/load_movie_awards.py
+python3 migrations/actors/load_actor_actorid.py
+python3 migrations/actors/load_movie_actors.py
+python3 migrations/audience/users_ratings.py
 python3 migrations/users/users.py
 
 echo "Migrations finished"
--- migrations/genres/genres.py
+++ migrations/genres/genres.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 def drop_tables(cursor, conn):
     cursor.execute("""
-        DROP TABLE IF EXISTS genre_links, movies, genres CASCADE;
+        DROP TABLE IF EXISTS genre_links, genres CASCADE;
     """)
     conn.commit()
 
@@ -36,17 +36,6 @@ def create_genre_links_table(cursor, conn):
     conn.commit()
 
 
-def create_movies_table(cursor, conn):
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS movies(
-            id INT PRIMARY KEY,
-            title VARCHAR(255),
-            genres VARCHAR(255)
-        );
-    """)
-    conn.commit()
-
-
 def insert_genres(cursor, conn):
     with open('data/ml-latest-small/genres.csv', 'r') as file:
         reader = csv.DictReader(file)
@@ -64,23 +53,6 @@ def insert_genres(cursor, conn):
             """, (id, genre_name, avg_rating, variance, total_ratings)) 
 
     conn.commit()
-
-
-def insert_movies(cursor, conn):
-    with open('data/ml-latest-small/movies.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            movie_id = int(row['movieId'])
-            title = row['title']
-            genres = row['genres'].split('|')
-
-            cursor.execute("""
-                INSERT INTO movies(id, title, genres)
-                VALUES (%s, %s, %s)
-            """, (movie_id, title, genres))
-
-        conn.commit()
-
 
 def insert_genre_links(cursor, conn):
     with open('data/ml-latest-small/genre_links.csv', 'r') as file:
@@ -121,14 +93,11 @@ def main():
         drop_tables(cursor, conn)
 
         # create tables
-        create_movies_table(cursor, conn)
         create_genres_table(cursor, conn)
         create_genre_links_table(cursor, conn)
         print('Finished creating all tables')
 
         # insert data
-        insert_movies(cursor, conn)
-        print('Finished inserting movies data.')
         insert_genres(cursor, conn)
         print('Finished inserting genres data')
         insert_genre_links(cursor, conn)
