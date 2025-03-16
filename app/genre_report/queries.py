beb256e5fea889bf371e7c12ab661d92b6ec7128
joshmiddlecote <joshuam@betsoftware.com>
2025-03-16 14:56:28 +0000

    point to postgres_db container

--- app/genre_report/queries.py
+++ app/genre_report/queries.py
@@ -8,7 +8,7 @@ load_dotenv()
 db_username = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 db_name = os.getenv("DB_NAME")
-DATABASE_URL = f"postgresql://{db_username}:{db_password}@localhost/{db_name}"
+DATABASE_URL = f"postgresql://{db_username}:{db_password}@postgres_db/{db_name}"
 
 # Database connection manager
 @contextmanager
